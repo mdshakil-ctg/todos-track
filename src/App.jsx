1d96fd9177eb848167a37f7d94a9427b3f67dc11
mdshakil-ctg <developer.shakil.ctg@gmail.com>
2025-02-13 22:17:54 +0600

    firebase initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-
-
+import {getAuth} from "firebase/auth"
+import app from "./Firebase/firebase.config"
 function App() {
-
+  const auth = getAuth(app)
+console.log(auth)
   return (
     <>
      Todos & Track
